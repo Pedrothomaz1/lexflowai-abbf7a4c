0cafea2c1fe7b2eb1d1429fee9e33aafdc7f17b2
Pedro Eduardo Thomaz <Pedrothomaz1@gmail.com>
2026-05-24 00:29:01 -0300

    chore: remove all repository files

--- src/components/Help/index.ts
+++
@@ -1,4 +0,0 @@
-export { HeroCards } from "./HeroCards";
-export { QuickGuides } from "./QuickGuides";
-export { FAQSection } from "./FAQSection";
-export { SupportContact } from "./SupportContact";
