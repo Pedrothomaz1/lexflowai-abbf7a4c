01655293ca920bacb69299491d38e2e946aa9e98
Pedro Eduardo Thomaz <Pedrothomaz1@gmail.com>
2026-05-23 21:54:20 -0300

    chore: remove all repository files

--- supabase/migrations/20260127001223_39ba1e17-5675-4d37-b0f7-12a960e2b2c8.sql
+++
@@ -1,6 +0,0 @@
--- Adicionar campos de dados bancários à tabela fornecedores
-ALTER TABLE fornecedores ADD COLUMN IF NOT EXISTS banco TEXT;
-ALTER TABLE fornecedores ADD COLUMN IF NOT EXISTS agencia TEXT;
-ALTER TABLE fornecedores ADD COLUMN IF NOT EXISTS conta TEXT;
-ALTER TABLE fornecedores ADD COLUMN IF NOT EXISTS pix TEXT;
-ALTER TABLE fornecedores ADD COLUMN IF NOT EXISTS titular_conta TEXT;
