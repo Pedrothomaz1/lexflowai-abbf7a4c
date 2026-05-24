5d260d4badf40ee0b2e946292d41218755121267
Pedro Eduardo Thomaz <Pedrothomaz1@gmail.com>
2026-05-23 22:04:30 -0300

    chore: remove all repository files

--- supabase/migrations/20260509235557_415685b7-2897-4f31-add6-89459ebe81bf.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM public.user_roles
-WHERE user_id = 'b6b35ada-b410-4c40-97c4-5f947c866b89';
-
-DELETE FROM public.organization_members
-WHERE user_id = 'b6b35ada-b410-4c40-97c4-5f947c866b89';
